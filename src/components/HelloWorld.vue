179ce3170112849128861893b7703df944a718a1
wangyongxin <wangyongxin546@163.com>
2024-01-16 11:19:26 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,6 +28,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button @click="throwError">Throw error</button>
   </div>
 </template>
 
@@ -36,6 +37,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    throwError() {
+      throw new Error('Sentry Error');
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,30 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Sentry from "@sentry/vue";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+Sentry.init({
+    app,
+    dsn: "https://9dd7e18ce001dcfc30ad8e93a99d911a@o4506545993089024.ingest.sentry.io/4506546005540864",
+    integrations: [
+        new Sentry.BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        }),
+        new Sentry.Replay(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+app.use(store).use(router).mount('#app')
